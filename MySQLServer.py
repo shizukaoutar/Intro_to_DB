d7143f3cd8bb47ecb51d1e4abf5d982086c34bf0
Kaoutar Qanouni <kaoutar.qanouni@gmail.com>
2025-06-29 15:17:20 +0000

    updated to include code to handle exceptions

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,13 +1,16 @@
 import mysql.connector
-
-mydb = mysql.connector.connect(
+try:
+    mydb = mysql.connector.connect(
     host="localhost",
     user="root",
     password="freepalestine710",
 )
-mycursor = mydb.cursor()
-mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store;") 
-print("Database 'alx_book_store' created successfully!")
+    mycursor = mydb.cursor()
+    mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store;") 
+    print("Database 'alx_book_store' created successfully!")
 
-mycursor.close()
-mydb.close()
+    mycursor.close()
+    mydb.close()
+except mysql.connector.Error as e:
+    print("Error: Could not connect to MySQL Server", e)
+    
