cf38d743667df2cf53d18e34be5cf22386989f74
Kaoutar Qanouni <kaoutar.qanouni@gmail.com>
2025-06-29 14:58:44 +0000

    bookstore database with SQL

---
+++ alx_book_store.sql
@@ -0,0 +1,38 @@
+CREATE DATABASE alx_book_store;
+
+CREATE TABLE books (
+    book_id INT PRIMARY KEY,
+    title VARCHAR(130),
+    author_id INT,
+    price DOUBLE,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES authors(author_id)
+)
+
+CREATE TABLE authors (
+    author_id INT PRIMARY KEY,
+    author_name VARCHAR(215),
+)
+    
+CREATE TABLE customers (
+    customer_id INT PRIMARY KEY, 
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
+)
+
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+)
+
+CREATE TABLE order_details (
+    orderdetail_id INT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES books(book_id)
+)
